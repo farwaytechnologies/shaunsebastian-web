35387bfd6ad85c7e74d20bc5084735921de45d37
Sino John Philip <sinojohnp@gmail.com>
2025-07-31 17:31:56 +0530

    Initial commit

--- frontend/src/Routes/PagesRoute.jsx
+++ frontend/src/Routes/PagesRoute.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+// Import your pages
 import Home from '../Pages/Home';
 import About from '../Pages/About';
 import Projects from '../Pages/Projects';
 import Contact from '../Pages/Contact';
 
-import AdminAuth from '../Pages/AdminPages/AdminAuth';
-import AdminDashboard from '../Pages/AdminPages/AdminDashboard';
+import AdminAuth from '../Admin/AdminAuth';
+import AdminDashboard from '../Admin/AdminDashboard';
+
 
 function PagesRoute() {
   return (
@@ -16,8 +18,8 @@ function PagesRoute() {
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/contact" element={<Contact />} />
-      <Route path="/admin/login" element={<AdminAuth />} />
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/login" element={<AdminAuth/>} />
     </Routes>
   );
 }
