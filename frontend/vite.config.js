54d23b8fa704500c6c228c2dbc58bcb8571610bf
shaunsebastian <shaunsebastian4@gmail.com>
2025-09-29 12:52:18 +0530

    redirect port fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  server: {
+    // This allows binding to the necessary network interface (0.0.0.0)
+    host: '0.0.0.0',
+
+    // This resolves the "Blocked request" error by allowing all hosts
+    allowedHosts: '*'},
+
+
   plugins: [react()],
+
 })
