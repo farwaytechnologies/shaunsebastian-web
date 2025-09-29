ce7d994ee1c89554ac6cae5d63dc1e62c7dc23ba
shaunsebastian <shaunsebastian4@gmail.com>
2025-09-29 12:48:27 +0530

    lower case error fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  // ... other configuration
-  server: {
-    host: '0.0.0.0', // This was the fix for the previous issue
-    port: process.env.PORT || 5173,
-    // Add this line to allow access from any host
-    allowedHosts: '*'
-  }
-});
+  plugins: [react()],
+})
